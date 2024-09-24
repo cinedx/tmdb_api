aeb7d29e71f100ad68785ecb7389ec8860a62a09
Almis <almisb90@gmail.com>
2024-09-24 20:17:48 +0300

    Revert "Removed unsupported comment"
    This reverts commit 6a9a9bd44a1f394d12868063fdd83983bc33c027.

--- lib/models/list_item.dart
+++ lib/models/list_item.dart
@@ -3,6 +3,7 @@ part of '../tmdb_api.dart';
 class ListItem {
   final MediaType mediaType;
   final int mediaId;
+  final String comment;
 
   ///- `mediaType`: Set the kind of media object are you adding. *Allowed Values: movie, tv*
   ///
@@ -10,16 +11,19 @@ class ListItem {
   const ListItem({
     /*required*/ required this.mediaId,
     this.mediaType = MediaType.movie,
+    this.comment = '',
   });
 
   ///create and returns a new `ListItem`. You can use it when want to change few feids.
   ListItem copyWith({
     MediaType? mediaType = MediaType.movie,
     int? mediaId,
+    String? comment = '',
   }) {
     return ListItem(
       mediaType: mediaType ?? this.mediaType,
       mediaId: mediaId ?? this.mediaId,
+      comment: comment ?? this.comment,
     );
   }
 
@@ -28,10 +32,11 @@ class ListItem {
   ///{
   ///   'media_type': mediaType,
   ///   'media_id': '$mediaId',
+  ///   'comment': comment
   ///}
   ///```
   Map<String, dynamic> _toMap() {
-    return {'media_type': _getMediaType(mediaType), 'media_id': '$mediaId'};
+    return {'media_type': _getMediaType(mediaType), 'media_id': '$mediaId', 'comment': comment};
   }
 
   String _getMediaType(MediaType mediaType) {
